10662647717e085ddc6b7c0d8b8d6dd7d11e877f
lukasja <lukasja@kth.se>
2024-11-22 22:12:51 +0100

    Made the code more modular and scalable for future work

---
+++ new/main.py
@@ -0,0 +1,156 @@
+# main.py
+import numpy as np
+import pygame
+import random
+import sys
+from player_agent import PlayerAgent
+from mimimax_agent import MinimaxAgent
+from testing.random_agent import RandomAgent
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+SQUARESIZE = 100
+RADIUS = int(SQUARESIZE/2 - 5)
+    
+
+
+def create_board(ROW_COUNT, COLUMN_COUNT):
+    return np.zeros((ROW_COUNT, COLUMN_COUNT))
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col, ROW_COUNT):
+    return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col, ROW_COUNT):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def winning_move(board, piece, ROW_COUNT, COLUMN_COUNT):
+    # Horizontal
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and \
+               board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # Vertical
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and \
+               board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # Positive diagonal
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and \
+               board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    # Negative diagonal
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and \
+               board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
+    return False
+
+def draw_board(board, screen, COLUMN_COUNT, ROW_COUNT, SQUARESIZE, RADIUS, height):
+    
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):     
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    pygame.display.update()
+
+def start_game(agent1, agent2, use_gui=True):
+    board = create_board(ROW_COUNT, COLUMN_COUNT)
+    game_over = False
+    turn = random.randint(0, 1)
+    
+    if use_gui:
+        pygame.init()
+        width = COLUMN_COUNT * SQUARESIZE
+        height = (ROW_COUNT+1) * SQUARESIZE
+        size = (width, height)
+        screen = pygame.display.set_mode(size)
+        draw_board(board, screen, COLUMN_COUNT, ROW_COUNT, SQUARESIZE, RADIUS, height)
+        myfont = pygame.font.SysFont("monospace", 75)
+    
+    while not game_over:
+
+        # check if game is over
+        if use_gui:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+        # set current agent and current piece (for printing)
+        current_agent = agent1 if turn == 0 else agent2
+        current_piece = 1 if turn == 0 else 2
+        
+        # player needs to be handled seperately, since we need input
+        if current_agent.__class__.__name__ == 'Player':
+            col = None
+            while col is None:
+                for event in pygame.event.get():
+                    if event.type == pygame.MOUSEMOTION:
+                        pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+                        posx = event.pos[0]
+                        pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+                        pygame.display.update()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        col = current_agent.get_move(board, event)
+        # if its not a player, use the agent interface. They all share find_move()
+        else:
+            col = current_agent.find_move(board)
+        
+        # now we have a column, so lets find out which row to place it
+        if is_valid_location(board, col, ROW_COUNT):
+            row = get_next_open_row(board, col, ROW_COUNT)
+            drop_piece(board, row, col, current_piece) # place the piece on the known row and column in our board
+
+            # if its a winning move
+            if winning_move(board, current_piece, ROW_COUNT, COLUMN_COUNT):
+                #print winner
+                print(f"Player {current_piece} , agent_name:{current_agent} wins!")
+                # show it visually
+                if use_gui:
+                    color = (255,0,0) if current_piece == 1 else (255,255,0)
+                    label = myfont.render(f"Player {current_piece} wins!", 1, color)
+                    screen.blit(label, (40,10))
+                
+                # set game_over to True in order to finish our main game loop.
+                game_over = True
+
+            # after the piece has been placed, show it visually 
+            if use_gui:
+                draw_board(board, screen, COLUMN_COUNT, ROW_COUNT, SQUARESIZE, RADIUS, height)
+                pygame.time.wait(500)  # Add delay to visualize moves
+
+            # not sure what this does, probably switch whose turn it is but seems like a weird way to do it
+            turn = (turn + 1) % 2
+
+        # if the game is over, we want to wait a while in order for the user to have time to see who won
+        if game_over and use_gui:
+            pygame.time.wait(3000)
+
+# main program
+if __name__ == "__main__":
+    first_player = Player()
+    second_player = RandomAgent()
+    start_game(first_player, second_player,True)
---
+++ new/mimimax_agent.py
@@ -0,0 +1,209 @@
+import numpy as np
+import random
+import pygame
+import sys
+import math
+
+
+# minimax class with alpha beta pruning
+
+class MinimaxAgent:
+    # default depth of 5, but can be changed
+    def __init__(self, depth=5):
+        self.depth = depth
+        self.ROW_COUNT = 6
+        self.COLUMN_COUNT = 7
+        self.WINDOW_LENGTH = 4
+    
+    # evaluate a window. A window is a all straight lines ( horizantal, vertical and diagonal)
+    def evaluate_window(self, window, piece):
+        score = 0
+        opp_piece = 1 if piece == 2 else 2
+
+        if window.count(piece) == 4:
+            score += 100
+        elif window.count(piece) == 3 and window.count(0) == 1:
+            score += 5
+        elif window.count(piece) == 2 and window.count(0) == 2:
+            score += 2
+
+        if window.count(opp_piece) == 3 and window.count(0) == 1:
+            score -= 4
+
+        return score
+
+    # give a score for the current position by looking through all possible directions
+    def score_position(self, board, piece):
+        score = 0
+
+        # Center column gets extra reward
+        center_array = [int(i) for i in list(board[:, self.COLUMN_COUNT//2])]
+        center_count = center_array.count(piece)
+        score += center_count * 3
+
+        # Horizontal
+        for r in range(self.ROW_COUNT):
+            row_array = [int(i) for i in list(board[r,:])]
+            for c in range(self.COLUMN_COUNT-3):
+                window = row_array[c:c+self.WINDOW_LENGTH]
+                score += self.evaluate_window(window, piece)
+
+        # Vertical
+        for c in range(self.COLUMN_COUNT):
+            col_array = [int(i) for i in list(board[:,c])]
+            for r in range(self.ROW_COUNT-3):
+                window = col_array[r:r+self.WINDOW_LENGTH]
+                score += self.evaluate_window(window, piece)
+
+        # Positive diagonal
+        for r in range(self.ROW_COUNT-3):
+            for c in range(self.COLUMN_COUNT-3):
+                window = [board[r+i][c+i] for i in range(self.WINDOW_LENGTH)]
+                score += self.evaluate_window(window, piece)
+
+        # Negative diagonal
+        for r in range(self.ROW_COUNT-3):
+            for c in range(self.COLUMN_COUNT-3):
+                window = [board[r+3-i][c+i] for i in range(self.WINDOW_LENGTH)]
+                score += self.evaluate_window(window, piece)
+
+        return score
+
+    # find all valid locations
+    def get_valid_locations(self, board):
+        valid_locations = []
+        for col in range(self.COLUMN_COUNT):
+            if board[self.ROW_COUNT-1][col] == 0:
+                valid_locations.append(col)
+        return valid_locations
+    
+    # Check if its a winning move or if the board is full ( in that case we can stop the search)
+    def is_terminal_node(self, board):
+        def winning_move(piece):
+            # Horizontal
+            for c in range(self.COLUMN_COUNT-3):
+                for r in range(self.ROW_COUNT):
+                    if board[r][c] == piece and board[r][c+1] == piece and \
+                       board[r][c+2] == piece and board[r][c+3] == piece:
+                        return True
+
+            # Vertical
+            for c in range(self.COLUMN_COUNT):
+                for r in range(self.ROW_COUNT-3):
+                    if board[r][c] == piece and board[r+1][c] == piece and \
+                       board[r+2][c] == piece and board[r+3][c] == piece:
+                        return True
+
+            # Diagonals
+            for c in range(self.COLUMN_COUNT-3):
+                for r in range(self.ROW_COUNT-3):
+                    if board[r][c] == piece and board[r+1][c+1] == piece and \
+                       board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                        return True
+
+            for c in range(self.COLUMN_COUNT-3):
+                for r in range(3, self.ROW_COUNT):
+                    if board[r][c] == piece and board[r-1][c+1] == piece and \
+                       board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                        return True
+            return False
+
+        return winning_move(1) or winning_move(2) or len(self.get_valid_locations(board)) == 0
+
+    # the main minimax logic. Lets look through it
+    # I think it alternates between maximizingPlayer and else, until alpha and beta are equal.
+    def minimax(self, board, depth, alpha, beta, maximizingPlayer):
+        #
+        valid_locations = self.get_valid_locations(board)
+        is_terminal = self.is_terminal_node(board)
+        if depth == 0 or is_terminal:
+
+            # if its a end node, check who won
+            if is_terminal:
+                if self.winning_move(board, 2):
+                    return (None, 100000000000000)
+                elif self.winning_move(board, 1):
+                    return (None, -10000000000000)
+                else:
+                    return (None, 0)
+            else:
+                return (None, self.score_position(board, 2))
+
+        if maximizingPlayer:
+            value = float('-inf')
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = self.get_next_open_row(board, col)
+                b_copy = board.copy()
+                self.drop_piece(b_copy, row, col, 2)
+                new_score = self.minimax(b_copy, depth-1, alpha, beta, False)[1]
+                if new_score > value:
+                    value = new_score
+                    column = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return column, value
+
+        else:
+            value = float('inf')
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = self.get_next_open_row(board, col)
+                b_copy = board.copy()
+                self.drop_piece(b_copy, row, col, 1)
+                new_score = self.minimax(b_copy, depth-1, alpha, beta, True)[1]
+                if new_score < value:
+                    value = new_score
+                    column = col
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return column, value
+
+    # find a open_row to place the piece vertically
+    def get_next_open_row(self, board, col):
+        for r in range(self.ROW_COUNT):
+            if board[r][col] == 0:
+                return r
+
+    def drop_piece(self, board, row, col, piece):
+        board[row][col] = piece
+
+    # this winning move is used by the main minimax algorrithm with self.winning_move()
+    # I think this is duplicated in is_terminal_node but its not a big issue since this one uses self
+
+    def winning_move(self, board, piece):
+        # Horizontal
+        for c in range(self.COLUMN_COUNT-3):
+            for r in range(self.ROW_COUNT):
+                if board[r][c] == piece and board[r][c+1] == piece and \
+                   board[r][c+2] == piece and board[r][c+3] == piece:
+                    return True
+
+        # Vertical
+        for c in range(self.COLUMN_COUNT):
+            for r in range(self.ROW_COUNT-3):
+                if board[r][c] == piece and board[r+1][c] == piece and \
+                   board[r+2][c] == piece and board[r+3][c] == piece:
+                    return True
+
+        # Diagonals
+        for c in range(self.COLUMN_COUNT-3):
+            for r in range(self.ROW_COUNT-3):
+                if board[r][c] == piece and board[r+1][c+1] == piece and \
+                   board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                    return True
+
+        for c in range(self.COLUMN_COUNT-3):
+            for r in range(3, self.ROW_COUNT):
+                if board[r][c] == piece and board[r-1][c+1] == piece and \
+                   board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                    return True
+        return False
+
+    # find_move
+    #the main function
+    def find_move(self, board):
+        col, _ = self.minimax(board, self.depth, float('-inf'), float('inf'), True)
+        return col
