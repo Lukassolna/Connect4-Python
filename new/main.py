01ce852fe844cb5de148e2bde7dd8790392d8718
thepoppe <pontus.morin@hotmail.se>
2024-12-15 19:40:16 +0100

    writing test results to a file added

--- new/main.py
+++ new/main.py
@@ -20,19 +20,24 @@ game_setting =(ROW_COUNT, COLUMN_COUNT, WIN_COUNT, WAIT_TIME)
 # main program
 if __name__ == "__main__":
     set_game_setting(game_setting)
-    mini1 = MinimaxAgent(game_setting, ORANGE, 3, "Mini")
-    mini2 = MinimaxAgent(game_setting, YELLOW, 5, "Mini")
+    mini1 = MinimaxAgent(game_setting, ORANGE, 1, "Mini")
+    mini2 = MinimaxAgent(game_setting, YELLOW, 2, "Mini")
+    mini3 = MinimaxAgent(game_setting, YELLOW, 3, "Mini")
+    mini4 = MinimaxAgent(game_setting, YELLOW, 4, "Mini")
     qlrn = Qlearning(game_setting,WHITE)
     qlrn2 = Qlearning(game_setting,GREEN)
     rnd = RandomAgent(game_setting, RED)
     user = PlayerAgent(GREEN)
-    qlrn.load("withhelper_vs_random.pkl")
+    qlrn.load("new/rnd.pkl")
     #qlrn2.load("vs_multi.pkl")
     #qlrn.epsilon = 0.9
+    #qlrn.epsilon = 0
     mr_q_wins = 0
     random_wins = 0
     draws = 0
-    num_games = 100000
+    num_games = 2000000
+    #with open("vsqlrn_samea.txt", "w") as file:
+    #    file.write(f"Testing qlrn a={qlrn.alpha}, g={qlrn.gamma} against qlrn2 a={qlrn2.alpha}, g={qlrn2.gamma}")
     for i in range(num_games):
         #print(i)
         winner = start_game(qlrn,rnd, True)
@@ -43,10 +48,16 @@ if __name__ == "__main__":
         else:
             random_wins += 1
        
-        if i % 1000  == 0 and i !=0:
-            print(f"After {i} games - Mr.Q: {mr_q_wins/(1000):.1%}, Random: {random_wins/(1000):.1%}, Draws: {draws/(10000):.1%} , epsilon {qlrn.epsilon}")
+        print_count = 1000
+        if i % print_count  == 0 and i !=0:
+            with open("vsqlrn_samea.txt", "a") as file:
+                file.write(f"After {i} games - qlrn: {mr_q_wins/print_count:.1%}, "
+                       f"qlrn2: {random_wins/print_count:.1%}, "
+                       f"Draws: {draws/print_count:.1%}, "
+                       f"epsilon: {qlrn.epsilon}\n")
+
             random_wins=0
             mr_q_wins=0
             draws=0 
-        if i % 1000 == 0:
-            qlrn.save(f'withhelper_vs_random.pkl')
+        #if i % 100000 == 0:
+        #    qlrn.save(f'qlrn.pkl')
