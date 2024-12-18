48f5b26f918f252877095c6b5770dabf28fa8779
lukasja <lukasja@kth.se>
2024-12-18 14:23:57 +0100

    cleaned up main.py

--- new/main.py
+++ new/main.py
@@ -5,13 +5,19 @@ from random_agent import RandomAgent
 from q import Qlearning
 from game import set_game_setting, start_game
 
-RED = (255,0,0)
-YELLOW = (255,255,0)
-GREEN = (0,255,0)
-ORANGE = (255,100,50)
-WHITE = (255,255,255)
-ROW_COUNT = 5
-COLUMN_COUNT = 6
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255) 
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+PURPLE = (128, 0, 128)
+CYAN = (0, 255, 255)
+WHITE = (255, 255, 255)
+PINK = (255, 192, 203)
+
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
 WIN_COUNT = 4
 WAIT_TIME = 1000
 game_setting =(ROW_COUNT, COLUMN_COUNT, WIN_COUNT, WAIT_TIME)
@@ -20,44 +26,14 @@ game_setting =(ROW_COUNT, COLUMN_COUNT, WIN_COUNT, WAIT_TIME)
 # main program
 if __name__ == "__main__":
     set_game_setting(game_setting)
-    mini1 = MinimaxAgent(game_setting, ORANGE, 1, "Mini")
+    mini1 = MinimaxAgent(game_setting, BLUE, 1, "Mini")
     mini2 = MinimaxAgent(game_setting, YELLOW, 2, "Mini")
-    mini3 = MinimaxAgent(game_setting, YELLOW, 3, "Mini")
-    mini4 = MinimaxAgent(game_setting, YELLOW, 4, "Mini")
-    qlrn = Qlearning(game_setting,WHITE)
-    qlrn2 = Qlearning(game_setting,GREEN)
+    mini3 = MinimaxAgent(game_setting, ORANGE, 3, "Mini")
+    mini4 = MinimaxAgent(game_setting, PURPLE, 4, "Mini")
+    mini5 = MinimaxAgent(game_setting, CYAN, 5, "Mini")
+    qlrn = Qlearning(game_setting, WHITE)
+    qlrn_w_helper = Qlearning(game_setting, PINK, helper=True)
     rnd = RandomAgent(game_setting, RED)
     user = PlayerAgent(GREEN)
-    qlrn.load("new/rnd.pkl")
-    #qlrn2.load("vs_multi.pkl")
-    #qlrn.epsilon = 0.9
-    #qlrn.epsilon = 0
-    mr_q_wins = 0
-    random_wins = 0
-    draws = 0
-    num_games = 2000000
-    #with open("vsqlrn_samea.txt", "w") as file:
-    #    file.write(f"Testing qlrn a={qlrn.alpha}, g={qlrn.gamma} against qlrn2 a={qlrn2.alpha}, g={qlrn2.gamma}")
-    for i in range(num_games):
-        #print(i)
-        winner = start_game(qlrn,rnd, True)
-        if winner == 1:
-            mr_q_wins += 1
-        elif winner == 10:
-            draws += 1
-        else:
-            random_wins += 1
-       
-        print_count = 1000
-        if i % print_count  == 0 and i !=0:
-            with open("vsqlrn_samea.txt", "a") as file:
-                file.write(f"After {i} games - qlrn: {mr_q_wins/print_count:.1%}, "
-                       f"qlrn2: {random_wins/print_count:.1%}, "
-                       f"Draws: {draws/print_count:.1%}, "
-                       f"epsilon: {qlrn.epsilon}\n")
-
-            random_wins=0
-            mr_q_wins=0
-            draws=0 
-        #if i % 100000 == 0:
-        #    qlrn.save(f'qlrn.pkl')
+  
+    winner = start_game(user, rnd, True)
